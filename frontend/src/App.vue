2d04c11b55d70bab40d92d7a9626170112b52c4e
Marcus <eyota@posteo.de>
2020-05-30 20:07:25 +0200

    better design for OverviewGrid

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,22 +3,10 @@
     <!-- App.vue -->
 
     <v-app>
-      <v-navigation-drawer app>
-        Navigation Drawer
-      </v-navigation-drawer>
-
-      <v-app-bar app>
-        <!-- -->
-      </v-app-bar>
-
       <!-- Sizes your content based upon application components -->
       <v-content>
         <overview-grid></overview-grid>
       </v-content>
-
-      <v-footer app>
-        <!-- -->
-      </v-footer>
     </v-app>
   </div>
 </template>
@@ -26,7 +14,6 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import OverviewGrid from "@/components/OverviewGrid.vue";
-
 
 @Component({
   components: {
--- frontend/src/components/OverviewGrid.vue
+++ frontend/src/components/OverviewGrid.vue
@@ -1,23 +1,45 @@
 <template>
-  <div class="grid">
-    <!-- Headers -->
-    <v-label>Datum</v-label>
-    <v-label>Wochentag</v-label>
-    <v-label>Kontostand</v-label>
-    <div v-for="m in 1" :key="m"></div>
+  <v-container class="grey lighten-5">
+    <v-row class="grey lighten-5">
+      <v-col cols="1">
+        <v-label>Datum</v-label>
+      </v-col>
+      <v-col cols="1">
+        <v-label>Wochentag</v-label>
+      </v-col>
+      <v-col cols="1">
+        <v-label>Kontostand</v-label>
+      </v-col>
+      <v-col cols="9">
+        <v-label>Transaktionen</v-label>
+      </v-col>
+    </v-row>
 
-    <template v-for="date in dates">
-      <div :key="'date' + formatDate(date)">{{ formatDate(date) }}</div>
-      <div :key="'day' + formatDate(date)">{{ dayOfWeek(date) }}</div>
-      <div :key=" 'balance' + formatDate(date)">{{ balanceOfDate(date) }}</div>
-
-      <v-chip
-        class="ma-2"
-        color="blue"
-        :key="'transaction' + formatDate(date)"
-      >{{transactionOfDate(date)}}</v-chip>
-    </template>
-  </div>
+    <v-row class="grey lighten-5" v-for="(date,i) in dates" :key="i">
+      <v-col cols="1">
+        <v-label>{{ formatDate(date) }}</v-label>
+      </v-col>
+      <v-col cols="1">
+        <v-label>{{ dayOfWeek(date) }}</v-label>
+      </v-col>
+      <v-col cols="1">
+        <v-label>{{ balanceOfDate(date) }}</v-label>
+      </v-col>
+      <v-col cols="9">
+        <v-slide-group multiple show-arrows>
+          <v-slide-item v-for="n in 15" :key="n" v-slot:default="{ active, toggle }">
+            <v-btn
+              :input-value="active"
+              active-class="purple white--text"
+              depressed
+              rounded
+              @click="toggle"
+            >Options {{ n }}</v-btn>
+          </v-slide-item>
+        </v-slide-group>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -47,7 +69,7 @@ export default class OverviewGrid extends Vue {
 
       this.transactionMap.set(
         date.format(this.dateFormatter),
-        "This is transaction {{i}}"
+        "This is transaction " + i
       );
 
       this.balanceMap.set(date.format(this.dateFormatter), i);
@@ -73,12 +95,4 @@ export default class OverviewGrid extends Vue {
 </script>
 
 <style scoped>
-.grid {
-  display: grid;
-  grid-template-columns: repeat(4, 150px);
-  grid-template-rows: repeat(32, auto);
-  text-align: center;
-  overflow: auto;
-  white-space: nowrap;
-}
 </style>
